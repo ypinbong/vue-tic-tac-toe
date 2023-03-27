ac47672118f6609ed1a60227284d1db285346097
yong <yongpinbong@gmail.com>
2023-03-27 22:10:41 +0800

    feat: optimized code

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -1,69 +1,50 @@
 <template>
-  <div style="margin-bottom: 10px;" v-if="winner">The winner is {{ winner }}!</div>
-  <div style="margin-bottom: 10px;" v-if="validTic.length === 9 && !winner">Draw!</div>
-  <div class="wrapper" >
-    <div v-for="index in 9" :key="index">
-      <button @click="handleClick(index)" :disabled="!!winner">{{ ticArr[index-1]?.state }}</button>
+  <div id="app">
+    <p v-if="winner === 'Tie'">It's a {{ winner }}</p>
+    <p v-if="!!winner && winner !== 'Tie'">Congratulations, Player {{ winner }} wins!</p>
+    <div class="wrapper">
+      <button v-for="(cell, index) in cells" :key="index" @click="handleClick(index)" :class="{'player-1': cell === 'X', 'player-2': cell === 'O'}">{{ cell }}</button>
     </div>
+    <button @click="resetGame">Reset</button>
   </div>
-  <button style="margin-top: 10px" @click="resetBoard">Reset</button>
 </template>
 
 <script lang="ts">
   export default {
     data(){
       return{
-        count: 0,
-        ticArr: [{
-          index: 0,
-          state: ""
-        }],
-        ticType: "X",
-        winner: "",
-        disableTic: false,
-      }
-    },
-    computed: {
-      validTic() {
-        return this.ticArr.filter(tic => tic.state)
+        cells: Array(9).fill(''),
+        currentPlayer: 'X',
+        winner: null || '',
       }
     },
     methods: {
-      handleClick(index:number){
-        this.ticArr[index-1] = {index: index, state: this.ticType};
-        this.ticType = this.ticType === "X" ? "O" : "X";
-        this.checkBoard()
-      },
-      checkBoard(){
-        if (this.validTic.length < 5) return;
-        const xMoves = this.validTic.filter(item => item.state === "X").map(item => item.index).sort((a, b) => a-b).join("");
-        const oMoves = this.validTic.filter(item => item.state === "O").map(item => item.index).sort((a, b) => a-b).join("");
-        let hasWinner;
-        if (xMoves.length >= 3) {
-          hasWinner = this.hasWinningCombo(xMoves)
-          if (hasWinner) {
-            this.winner = "X";
-          }
-        }
-        if (oMoves.length >= 3) {
-          hasWinner = this.hasWinningCombo(oMoves)
-          if (hasWinner) {
-            this.winner = "O";
-          }
+      handleClick(index: number) {
+        if (!this.cells[index] && !this.winner) {
+          this.cells[index] = this.currentPlayer;
+          this.checkWinner();
+          this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
         }
       },
-      hasWinningCombo(str: string): boolean{
-        const winningCombos = ['123', '456', '789', '147', '258', '369', '159', '357'];
-        const res = winningCombos.some(combo => str.includes(combo));
-        console.log("TicTacToe.vue ~ line 56: res", res);
-        return res;
+      checkWinner() {
+        const winningCombos = ['012', '345', '678', '036', '147', '258', '048', '246'];
+        for (const combo of winningCombos) {
+          const [a, b, c] = combo.split('');
+          if (this.cells[a] && this.cells[a] === this.cells[b] && this.cells[b] === this.cells[c]) {
+            this.winner = this.currentPlayer;
+            return;
+          }
+        }
+        if (!this.cells.includes('')) {
+          this.winner = 'Tie';
+        }
       },
-      resetBoard(){
-        this.ticArr = [];
-        this.ticType = "X";
-        this.winner = "";
+      resetGame() {
+        this.cells.fill('');
+        this.currentPlayer = 'X';
+        this.winner = null || '';
       },
-    }
+    },
   }
 </script>
 
