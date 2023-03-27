1f3b96e3690532912585caffc71e1c8d7b324a6a
yong <yongpinbong@gmail.com>
2023-03-27 22:27:08 +0800

    feat: implement tictactoe using vue3 script setup

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -1,51 +1,46 @@
 <template>
-  <div id="app">
-    <p v-if="winner === 'Tie'">It's a {{ winner }}</p>
-    <p v-if="!!winner && winner !== 'Tie'">Congratulations, Player {{ winner }} wins!</p>
-    <div class="wrapper">
-      <button v-for="(cell, index) in cells" :key="index" @click="handleClick(index)" :class="{'player-1': cell === 'X', 'player-2': cell === 'O'}">{{ cell }}</button>
-    </div>
-    <button @click="resetGame">Reset</button>
+  <p v-if="winner==='Tie'">It's a {{ winner }}</p>
+  <p v-if="!!winner && winner!=='Tie'">Congratulations, Player {{ winner }} wins!</p>
+  <div class="wrapper">
+    <button v-for="(cell, index) in cells" :key="index" @click="handleClick(index)" :class="{'player-1': cell === 'X', 'player-2': cell === 'O'}">{{ cell }}</button>
   </div>
+  <button @click="resetGame">Reset</button>
 </template>
 
-<script lang="ts">
-  export default {
-    data(){
-      return{
-        cells: Array(9).fill('') as string[],
-        currentPlayer: 'X',
-        winner: null || '',
-      }
-    },
-    methods: {
-      handleClick(index: number) {
-        if (!this.cells[index] && !this.winner) {
-          this.cells[index] = this.currentPlayer;
-          this.checkWinner();
-          this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
-        }
-      },
-      checkWinner() {
-        const winningCombos = ['012', '345', '678', '036', '147', '258', '048', '246'];
-        for (const combo of winningCombos) {
-          const [a, b, c] = combo.split('');
-          if (this.cells[a as unknown as number] && this.cells[a as unknown as number] === this.cells[b as unknown as number] && this.cells[b as unknown as number] === this.cells[c as unknown as number]) {
-            this.winner = this.currentPlayer;
-            return;
-          }
-        }
-        if (!this.cells.includes('')) {
-          this.winner = 'Tie';
-        }
-      },
-      resetGame() {
-        this.cells.fill('');
-        this.currentPlayer = 'X';
-        this.winner = null || '';
-      },
-    },
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const cells = ref<string[]>(Array(9).fill(''));
+const currentPlayer = ref<string>('X');
+const winner = ref<string | null>(null);
+
+function handleClick(index: number) {
+  if (!cells.value[index] && !winner.value) {
+    cells.value[index] = currentPlayer.value;
+    checkWinner();
+    currentPlayer.value = currentPlayer.value === 'X' ? 'O' : 'X';
   }
+}
+
+function checkWinner() {
+  const winningCombos = ['012', '345', '678', '036', '147', '258', '048', '246'];
+  for (const combo of winningCombos) {
+    const [a, b, c] = combo.split('');
+    if (cells.value[+a] && cells.value[+a] === cells.value[+b] && cells.value[+b] === cells.value[+c]) {
+      winner.value = currentPlayer.value;
+      return;
+    }
+  }
+  if (!cells.value.includes('')) {
+    winner.value = 'Tie';
+  }
+}
+
+function resetGame() {
+  cells.value.fill('');
+  currentPlayer.value = 'X';
+  winner.value = null;
+}
 </script>
 
 <style scoped>
@@ -54,7 +49,6 @@
   grid-template-columns: repeat(3, 1fr);
 }
 button{
-  color: white;
   width: 80px;
   height: 80px;
 }
